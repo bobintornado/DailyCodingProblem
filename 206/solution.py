150028f73d3067f6d4b5ef076a8f1e5e5fd3bc40
Bob Cao <bobintornado@gmail.com>
2019-11-07 10:41:23 +0800

    fix 206

--- 206/solution.py
+++ 206/solution.py
@@ -3,16 +3,28 @@ def permute(array, permutation):
         p = permutation[i]
         j = i
 
-        while p != j:
+        # stop when (use and since both must hold for the loop)
+        # 1. p is -1, means it has been settled before or
+        # 2. p is original i, means the loop has closed
+        while p != -1 and p != i:
+            # swap
             array[j],array[p] = array[p],array[j]
+            # permutation[j] is good 
+            permutation[j] = -1
+            # prepare for next swap
             j = p
+            # next p 
             p = permutation[p]
-            permutation[j] = i      
+
+        # end loop fix
+        permutation[j] = -1
 
     return array
 
 print permute(["a", "b", "c"],[2, 1, 0])
 print permute(["a", "b", "c", "d"], [3, 0, 2, 1])
+print permute(["a", "b", "c", "d", "e"], [4, 3, 2, 0, 1])
 
 # ['c', 'b', 'a']
 # ['d', 'a', 'c', 'b']
+# ['e', 'd', 'c', 'a', 'b']
